a68d112af9b7c032abc500214198c4f02797ed58
Ritik <ritikpr307@gmail.com>
2020-06-05 18:42:47 +0530

    Updates

---
+++ precache-manifest.b9672532026aebc6047423e5cb7c3395.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59b3ebc3fbb2142ef662d7103f57948e",
+    "url": "/video_search/index.html"
+  },
+  {
+    "revision": "83d4b812bdb5857f49d4",
+    "url": "/video_search/static/css/main.9be4cdc9.chunk.css"
+  },
+  {
+    "revision": "0849b81e7d08d93947cf",
+    "url": "/video_search/static/js/2.9f456e0f.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/video_search/static/js/2.9f456e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d4b812bdb5857f49d4",
+    "url": "/video_search/static/js/main.08cb357b.chunk.js"
+  },
+  {
+    "revision": "f771f2193e29defdd5df",
+    "url": "/video_search/static/js/runtime-main.276d5732.js"
+  }
+]);
